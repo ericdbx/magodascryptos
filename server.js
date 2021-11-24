13e5055c7758c9cd74d9baf6e2600d6bdd0c53c1
Eric Braga <ericdbx@gmail.com>
2021-11-24 12:29:25 -0300

    3000 again

--- server.js
+++ server.js
@@ -108,6 +108,6 @@ app.use(function(req, res, next) {
 
   
  
-  app.listen(process.env.port || 8080);
+  app.listen(process.env.port || 3000);
 
- console.log('Running at Port 8080');
+ console.log('Running at Port 3000');
